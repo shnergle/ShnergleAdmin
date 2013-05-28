26eba9f70d25d50704c206fd6189662626463e9b
Adam Hani Schakaki <adam@schakaki.net>
2013-05-28 16:36:53 +0100

    Simple Login

---
+++ footer.php
@@ -0,0 +1,9 @@
+		</div>
+		<script src="js/jquery.js" type="text/javascript">
+</script><script src="js/bootstrap.js" type="text/javascript">
+</script><script type="text/javascript" src="http://platform.twitter.com/widgets.js">
+</script><script src="js/holder/holder.js" type="text/javascript">
+</script><script src="js/application.js" type="text/javascript">
+</script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$type = 'normal';
+
+if (isset($_GET['action']) && $_GET['action'] == 'auth') {
+	if ($_POST['username'] == 'shnergle' && $_POST['password'] == '$Hnergle1') {
+		$_SESSION['auth'] = true;
+	} else {
+		$type = 'signin';
+		require_once 'header.php';
+		require_once 'signin.html';
+	}
+} elseif (isset($_GET['action']) && $_GET['action'] == 'out') {
+	session_unset();
+	session_destroy();
+	header('Location: /');
+} elseif (!isset($_SESSION['auth'])) {
+	$type = 'signin';
+	require_once 'header.php';
+	require_once 'signin.html';
+} else {
+	require_once 'header.php';
+	require_once 'index.html';
+}
+
+require_once 'footer.php';
+?>
