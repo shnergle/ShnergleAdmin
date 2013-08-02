f8dfefa6df1ece5cb1901258656a20d1e7421fd8
Adam Hani Schakaki <adam@schakaki.net>
2013-08-02 10:10:17 +0100

    .

--- controllers/Venues.php
+++ controllers/Venues.php
@@ -6,7 +6,7 @@ class Venues extends Controller {
       $search[] = 'name LIKE \'%' . str_replace(' ', '%', $this->form['venue-search']) . '%\'';
 		$this->entries = $this->db_query_all(null, null, $search);
     $this->page = empty($this->params['page']) ? 1 : $this->params['page'];
-    $this->pages = ceil($this->db_count() / ENTRIES_PER_PAGE);
+    $this->pages = ceil(count($this->entries) / ENTRIES_PER_PAGE);
 		$this->render();
 	}
 }
