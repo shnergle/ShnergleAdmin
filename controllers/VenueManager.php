6234ca7be45624ee7ab10aa7045705394cbcc883
Adam Hani Schakaki <adam@schakaki.net>
2013-08-06 14:47:28 +0100

    .

--- controllers/VenueManager.php
+++ controllers/VenueManager.php
@@ -4,7 +4,6 @@ require_once 'Mail.php';
 class VenueManagers extends Controller {
 	function edit() {
 		$this->entry = $this->db_query_one(null, null, array('user_id = ' . $this->params['user_id'], 'venue_id = ' . $this->params['venue_id']));
-    unset($this->entry['id']);
     $this->render();
 	}
 }
