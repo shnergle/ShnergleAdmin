bb49a5a1a0e96dd1c97876099daea3e310cb7a9c
Adam Hani Schakaki <adam@schakaki.net>
2013-10-03 21:06:41 +0100

    add dump tool

---
+++ dump.php
@@ -0,0 +1,33 @@
+<?php
+// http://daipratt.co.uk/exporting-data-sql-server-2005-express-csv-php-mssql/
+//MSSQL connection string details.
+require_once 'configs/config.php';
+session_start();
+if (empty($_SESSION['auth'])) return;
+$coninfo = array('Database' => DB_DB, 'UID' => DB_USER, 'PWD' => DB_PASS);
+$db = sqlsrv_connect(DB_SERVER, $coninfo);
+$sql = "SELECT * FROM target_database_table_name";
+$results = mssql_query($sql, $db);
+//Generate CSV file - Set as MSSQL_ASSOC as you don't need the numeric values.
+while ($l = mssql_fetch_array($results, MSSQL_ASSOC)) {
+    foreach($l AS $key => $value){
+        //If the character " exists, then escape it, otherwise the csv file will be invalid.
+        $pos = strpos($value, '"');
+        $pos1 = strpos($value, ',');
+        if ($pos !== false) {
+            $value = str_replace('"', '\"', $value);
+        }
+        if ($pos1 !== false) {
+            $value = str_replace(',', '\,', $value);
+        }
+        $out .= '"'.$value.'",';
+    }
+    $out .= "\n";
+}
+mssql_free_result($results);
+mssql_close($db);
+// Output to browser with the CSV mime type
+header("Content-type: text/x-csv");
+header("Content-Disposition: attachment; filename=table_dump.csv");
+echo $out;
+?>
